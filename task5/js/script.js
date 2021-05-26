09cc805db0ae3e151982e24e3a68940100154a9e
Vladimir <vladimir@rupor.agency>
2021-05-26 19:38:12 +0300

    done task5

--- task5/js/script.js
+++ task5/js/script.js
@@ -1,12 +1,56 @@
 class ElectricalDevices {
-    constructor(name) {
-        this.name = name;
+    constructor(name, turn) {
+        this.name = name,
+            this.turn = turn
     }
-    getPriority(priority){
+    getPriority(priority) {
         console.log(`Priority is ${priority}`);
+    }
+    devicesState() {
+        if (this.turn === true) {
+            console.log(`${this.name} включен!`);
+        } else {
+            console.log(`${this.name} выключен!`);
+        }
+    }
+
+}
+
+class Lamp extends ElectricalDevices {
+
+    powerСonsumption(wat, ampere) {
+        super.getPriority(1);
+        super.devicesState();
+        if (this.turn === true) {
+            console.log(wat * ampere);
+        }else{
+            console.log('Включите лампу');
+        }
+
+
     }
 }
 
-const electronic = new ElectricalDevices('test');
+class Pc extends ElectricalDevices {
 
-electronic.getPriority(1);
+    protectCpu(cpuLoad) {
+        super.getPriority(2);
+        super.devicesState();
+        if (this.turn === true) {
+            if (cpuLoad >= 100) {
+                console.log('ПЕРЕЗАГРУЗКА');
+            } else {
+                console.log('Работаем');
+            }
+        } else {
+            console.log('Компьютер не работает');
+        }
+    }
+
+
+}
+
+const lamp = new Lamp('Лампа', true);
+const pc = new Pc('Компьютер', true);
+lamp.powerСonsumption(100, 50);
+pc.protectCpu(100);

66c5c6774befb1c05bc5149d29506e3c752b5746
Vladimir <vladimir@rupor.agency>
2021-05-26 16:31:06 +0300

    done task 1

---
+++ task1/js/script.js
@@ -0,0 +1,13 @@
+const person = {
+
+    city: "Moscow",
+    index:'f213',
+    addres:'Mironova 5'
+   
+   }
+function viewObject(obj){
+    console.log(Object.keys(obj));
+    console.log(Object.values(obj));
+    console.log(Object.entries(obj));
+}
+viewObject(person);
